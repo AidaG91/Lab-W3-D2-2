07c53bec5ad99556bccff07fa6841d49e4c83e36
AidaG91 <aida.garmus@gmail.com>
2025-06-17 19:08:27 +0200

    Iteration 3

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 // Iteration 1: Names and Input
-let hacker1 = prompt ("Driver, what's your name?");
-let hacker2 = prompt ("Navigator, what's your name?");
+let hacker1 = prompt("Driver, what's your name?");
+let hacker2 = prompt("Navigator, what's your name?");
 
 console.log(`The driver's name is ${hacker1}.`);
 console.log(`The navigator's name is ${hacker2}.`);
 
-
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
- console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
 } else if (hacker1.length < hacker2.length) {
-console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
 } else {
-console.log(`Wow, you both have equally long names, ${hacker1.length} characters.`)
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters.`
+  );
 }
 
-
 // Iteration 3: Loops
+
+// Iteration 3.1
+
+let driverNameSpaced = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  let char = hacker1[i];
+  let upperChar = char.toUpperCase();
+  driverNameSpaced = driverNameSpaced + upperChar + " ";
+}
+
+driverNameSpaced = driverNameSpaced.trim();
+console.log(driverNameSpaced);
+
+// Iteration 3.2
+
+let chars = hacker2.split("");
+chars.reverse();
+let reversedName = chars.join("");
+
+console.log(reversedName);
+
+// Iteration 3.3
+
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first, definitely.");
+} else {
+  console.log("What?! You both have the same name? ");
+}
